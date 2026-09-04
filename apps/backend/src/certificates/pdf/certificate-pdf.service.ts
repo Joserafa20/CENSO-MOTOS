1731559c9f937db48444bc4843f1db7b18e33e0f
Joserafa20 <joserafaels762@gmail.com>
2026-09-04 15:06:00 -0500

    fix: add rxjs, @types/pdfkit, fix pdfkit import

--- apps/backend/src/certificates/pdf/certificate-pdf.service.ts
+++ apps/backend/src/certificates/pdf/certificate-pdf.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as PDFDocument from 'pdfkit';
+import PDFDocument from 'pdfkit';
 import * as QRCode from 'qrcode';
 import * as crypto from 'crypto';
 
